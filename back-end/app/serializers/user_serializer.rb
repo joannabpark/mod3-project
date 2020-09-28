720b53e3337a23752940c33458c26390f5a9fdd8
joannabpark <joannabpark1@gmail.com>
2020-09-28 15:43:33 -0500

    backend

--- back-end/app/serializers/user_serializer.rb
+++ back-end/app/serializers/user_serializer.rb
@@ -1,4 +1,7 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :age, :image_url, :attracted_to, :occupation, :interests, :location, :phone_number, :email_address, :veggie_type
+  has_many :initiated_matches
+  has_many :accept_matches
+
 end
